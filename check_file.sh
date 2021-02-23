a9a27ad7fb608e12c900ab1675c4c5111ec570af
Veternal1226 <q36084117@gs.ncku.edu.tw>
2021-02-23 16:48:42 +0800

    Implement source code comparison

--- check_file.sh
+++ check_file.sh
@@ -5,32 +5,10 @@ x=0
 
 # source file
 
-for source_file in $( ls ./code); do
- 
-    if [ -e code ];
-    then
-        echo " $source_file exist "
-    else
-        echo " $source_file does not exist "
-    fi
+for source_file in $( ls ./src); do
+    echo "checking $source_file ..."
+    diff ./src/$source_file ./code/$soure_file
 done
-
-# source file
-
-# common_file
-
-#for common_file in $( ls ../Source/Lab-04/Code/common);do
- 	
-   #if [ -e Code/common ];
-   #then 
-   #    echo " $common_file directory exist "
-   #else   
-   #    echo "$common_file directory not exist"
-   #fi
-#done
-
-#echo "The script is now complete"
-
 exit 0
 
 
